2586d7939e1197da52203513338586f8d5088076
Viktor Jannicke <viktor@jannicke.eu>
2026-07-26 16:47:31 +0200

    Align development and samples with cs-webui

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "WebUIToolkit development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    cs-webui.url = "github:ViktorJannicke/cs-webui";
+  };
+
+  outputs =
+    { nixpkgs, cs-webui, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          inherit (pkgs) lib;
+          csWebUiNative = cs-webui.packages.${system}.webui-native;
+          nativeLibraryName =
+            if pkgs.stdenv.hostPlatform.isDarwin then
+              "libwebui-2.dylib"
+            else
+              "libwebui-2.so";
+          linuxRuntimePackages = with pkgs; lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+            chromium
+            gtk3
+            webkitgtk_4_1
+            xvfb
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              dotnet-sdk_10
+              nodejs_24
+              powershell
+
+              # Required by the repository's Native AOT verification.
+              clang
+              zlib
+            ] ++ linuxRuntimePackages;
+
+            DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+            DOTNET_NOLOGO = "1";
+            DisableImplicitLibraryPacksFolder = "true";
+
+            shellHook = ''
+              # Keep interactive restores separate from the verification scripts,
+              # which intentionally build several different packages at version 1.0.0.
+              export NUGET_PACKAGES="$PWD/.direnv/nuget"
+              export CSWEBUI_NATIVE_LIBRARY="${csWebUiNative}/lib/${nativeLibraryName}"
+              ${lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+                export LD_LIBRARY_PATH="${lib.makeLibraryPath linuxRuntimePackages}:$LD_LIBRARY_PATH"
+                export WEBUI_BROWSER_PATH="${pkgs.chromium}/bin/chromium"
+              ''}
+
+              waveGFeed="$PWD/artifacts/wave-g/packages/nuget/feed"
+              if [[ -d "$waveGFeed" ]]; then
+                export RestoreAdditionalProjectSources="$waveGFeed"
+              fi
+              unset waveGFeed
+            '';
+          };
+        }
+      );
+    };
+}
